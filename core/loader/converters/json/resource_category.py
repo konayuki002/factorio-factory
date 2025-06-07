c2d2a50a5dbb5fa1db40142e5fee2310716249ca
Yukihide Ochi <konayuki0455@gmail.com>
2025-06-07 18:29:48 +0900

    Add resource_category json converter

---
+++ core/loader/converters/json/resource_category.py
@@ -0,0 +1,27 @@
+from core.loader.converters.base import BaseConverter  # 共通ユーティリティ
+from core.loader.registry import register
+from core.utils.lua_parser import parse_lua_file
+
+
+@register("json:resource_category")
+class ResourceCategoryJsonConverter(BaseConverter):
+    """
+    資源カテゴリに関するLua -> JSONファイルの変換.
+    具体的には、以下のファイルを処理します:
+    raw/resource-category.lua -> intermediate/resource_category.json
+    """
+
+    dependencies = []
+    lua_path = "resource-category.lua"
+    json_categories_path = "resource_category.json"
+
+    def load(self):
+        # 1) Lua -> dict
+        lua_file = f"{self.raw_dir}/{self.lua_path}"
+        data = parse_lua_file(lua_file)
+
+        # 2) 必要なら前処理
+
+        # 3) dict -> JSON
+        json_path = f"{self.intermediate_dir}/{self.json_categories_path}"
+        self.dump_json(data, json_path)
