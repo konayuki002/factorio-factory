326189a4ddb0eab1e306fea3467f465d8c6362ca
Yukihide Ochi <konayuki0455@gmail.com>
2025-06-14 23:31:00 +0900

    Add property to refer data via pydantic

--- core/models/models/material.py
+++ core/models/models/material.py
@@ -1,12 +1,14 @@
 from typing import Union
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, computed_field, field_validator
+from sympy import Integer
 
 from core.enums.material import Material as MaterialEnum
 from core.models.allowed.fluid import fluid_allowed
 from core.models.allowed.item import item_allowed
 from core.models.allowed.resource import resource_allowed
 from core.models.allowed.technology import technology_allowed
+from core.models.data.item import STACK_SIZE
 
 
 class MaterialBase(BaseModel):
@@ -19,6 +21,17 @@ class Item(MaterialBase):
         if v not in item_allowed:
             raise ValueError(f"Invalid item: {v}")
         return v
+
+    @property
+    @computed_field(return_type=Integer, repr=False)
+    def stack_size(self) -> Integer:
+        """
+        アイテムのスタックサイズを返す.
+        """
+        try:
+            return STACK_SIZE[self.id]
+        except KeyError as e:
+            raise ValueError(f"Invalid item stack size for: {self.id}") from e
 
 
 class Fluid(MaterialBase):
