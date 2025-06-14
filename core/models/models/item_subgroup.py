da10ab71235a963a4c6d5927680b6bb5aec3f9d9
Yukihide Ochi <konayuki0455@gmail.com>
2025-06-14 23:27:49 +0900

    Add property to refer data via pydantic

--- core/models/models/item_subgroup.py
+++ core/models/models/item_subgroup.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 
 from core.enums.item_subgroup import ItemSubgroup as ItemSubgroupEnum
+from core.models.data.item_subgroup import GROUP_OF_SUBGROUP
+from core.models.models.item_group import ItemGroup
 
 
 class ItemSubgroup(BaseModel):
     id: ItemSubgroupEnum
+
+    class Config:
+        frozen = True  # ハッシュ化・キャッシュしやすくする
+
+    @property
+    @computed_field(return_type=ItemGroup, repr=False)
+    def group(self) -> ItemGroup:
+        """
+        アイテムサブグループに対応するアイテムグループを返す.
+        """
+        try:
+            return ItemGroup(id=GROUP_OF_SUBGROUP[self.id])
+        except KeyError as e:
+            raise ValueError(f"Invalid item subgroup: {self.id}") from e
