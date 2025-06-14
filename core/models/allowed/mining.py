bc5e9424a2510c13fcbf30769cdbf429356e823a
Yukihide Ochi <konayuki0455@gmail.com>
2025-06-14 21:09:15 +0900

    Add MiningAllowedConverter

---
+++ core/models/allowed/mining.py
@@ -0,0 +1,10 @@
+from enums.operation import Operation
+
+mining_allowed: set[Operation] = {
+    Operation.MiningIronOre,
+    Operation.MiningCopperOre,
+    Operation.MiningCoal,
+    Operation.MiningStone,
+    Operation.MiningUraniumOre,
+    Operation.MiningCrudeOil,
+}
